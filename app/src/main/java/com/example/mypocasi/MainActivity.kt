eb073fcd07569f2cad1c202820666b5dd0c60957
Honza <honza2600@seznam.cz>
2020-12-28 22:42:43 +0100

    funkcni tlacitko vyhledat v menu

--- app/src/main/java/com/example/mypocasi/MainActivity.kt
+++ app/src/main/java/com/example/mypocasi/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.mypocasi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        // get reference to button
+        val btnVyhledat = findViewById(R.id.btnVyhledat) as Button
+        // set on-click listener
+        btnVyhledat.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            val intent = Intent(this, LookWeatherActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
