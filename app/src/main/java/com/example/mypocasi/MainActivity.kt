6a6fb7cae5da1b896aec1f0c684e6b8c730237e6
Honza <honza2600@seznam.cz>
2020-12-29 17:13:40 +0100

    pridani funkce na tlacitko zpet

--- app/src/main/java/com/example/mypocasi/MainActivity.kt
+++ app/src/main/java/com/example/mypocasi/MainActivity.kt
@@ -4,28 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
+    private var backPressedTime = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
-        // get reference to button
         val btnVyhledat = findViewById(R.id.btnVyhledat) as Button
-        // set on-click listener
         btnVyhledat.setOnClickListener {
-            // your code to perform when the user clicks on the button
             val intent = Intent(this, LookWeatherActivity::class.java)
             startActivity(intent)
         }
 
-
         val btnKonec = findViewById(R.id.btnKonec) as Button
-
         btnKonec.setOnClickListener{
             finish()
         }
+    }
+
+    override fun onBackPressed() {
+        if (backPressedTime + 3000 > System.currentTimeMillis()) {
+            super.onBackPressed()
+        } else {
+            Toast.makeText(applicationContext, "Opětovným stisknutím tlačítka zpět aplikaci ukončíte", Toast.LENGTH_SHORT).show()
+        }
+        backPressedTime = System.currentTimeMillis()
     }
 }
