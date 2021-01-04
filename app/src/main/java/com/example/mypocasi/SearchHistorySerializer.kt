3a7f43908efbbd870d2b877cf00f7f3fc691a439
Honza <honza2600@seznam.cz>
2021-01-04 22:03:05 +0100

    pokus o proto datastore

---
+++ app/src/main/java/com/example/mypocasi/SearchHistorySerializer.kt
@@ -0,0 +1,22 @@
+package com.example.mypocasi
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+
+/*class SearchHistorySerializer : Serializer<SearchHistory>{
+    override fun readFrom(input: InputStream): SearchHistory{
+        try {
+            return SearchHistory.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+
+    }
+
+    override fun writeTo(t: SearchHistory, output: OutputStream) = t.writeTo(output)
+
+
+}*/
