88a3b86cec8be291fca51f0e68c034eb0182be36
Honza <honza2600@seznam.cz>
2021-01-09 14:44:20 +0100

    oprava ukládání dat

--- app/src/main/java/com/example/mypocasi/ProtoRepository.kt
+++ app/src/main/java/com/example/mypocasi/ProtoRepository.kt
@@ -2,19 +2,20 @@ package com.example.mypocasi
 
 import android.content.Context
 import android.util.Log
-import androidx.datastore.core.DataStore
+import androidx.datastore.DataStore
+import androidx.datastore.createDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import java.io.IOException
 
-/*
 class ProtoRepository(context: Context){
 
     private val dataStore: DataStore<SearchHistory> = context.createDataStore(
-                    fileName = "search_history",
-                    serializer = SearchHistorySerializer)
+                    fileName = "search_history.pb",
+                    serializer = SearchHistorySerializer
+    )
 
-    val searchHistoryRead: Flow<SearchHistory> = dataStore.data
+   val searchHistoryRead: Flow<SearchHistory> = dataStore.data
             .catch { exception->
                 if (exception is IOException){
                     Log.d("Error", exception.message.toString())
@@ -24,12 +25,10 @@ class ProtoRepository(context: Context){
                     throw exception
                 }
             }
-
-    suspend fun update(pocet: int){
+   suspend fun update(mesto: String){
         dataStore.updateData { preference->
-            preference.toBuilder().setPocet(pocet).build()
+            preference.toBuilder().setNazevMesta(mesto).build()
         }
-    }
+   }
+}
 
-
-}*/
--- app/src/main/java/com/example/mypocasi/SearchHistorySerializer.kt
+++ app/src/main/java/com/example/mypocasi/SearchHistorySerializer.kt
@@ -1,23 +1,23 @@
 package com.example.mypocasi
 
-import androidx.datastore.core.CorruptionException
-import androidx.datastore.core.Serializer
+import androidx.datastore.CorruptionException
+import androidx.datastore.Serializer
 import com.google.protobuf.InvalidProtocolBufferException
 import java.io.InputStream
 import java.io.OutputStream
 
-object SearchHistorySerializer : Serializer<SearchHistory>{
-    override val defaultValue: SearchHistory = SearchHistory.getDefaultInstance()
+object SearchHistorySerializer : Serializer<SearchHistory> {
+    //override val defaultValue: SearchHistory = SearchHistory.getDefaultInstance()
+
     override fun readFrom(input: InputStream): SearchHistory{
         try {
             return SearchHistory.parseFrom(input)
         } catch (exception: InvalidProtocolBufferException) {
             throw CorruptionException("Cannot read proto.", exception)
         }
-
     }
 
-    override fun writeTo(t: SearchHistory, output: OutputStream) = t.writeTo(output)
-
-
+    override fun writeTo(t: SearchHistory, output: OutputStream){
+        t.writeTo(output)
+    }
 }
